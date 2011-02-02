a0d6a0a8ac84ce890894f9dc11227f8b79cd5a0d
Hypersonic <joshhofing@gmail.com>
2011-02-01 23:23:23 -0500

    Added door.cpp, with some basic io code.

---
+++ door.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+int main () {
+	string i = "";
+	cout << "Type something!" <<  endl;
+	cin >> i;
+	cout << "You typed: " << i << endl;
+	return 0;
+}
