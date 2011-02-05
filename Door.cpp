778a2687b7f601fcd554491ff80b03cc6174de45
Hypersonic <joshhofing@gmail.com>
2011-02-05 18:50:44 -0500

    The problem appears to be here. I can't figure out a fix though.

--- Door.cpp
+++ Door.cpp
@@ -16,19 +16,26 @@
  */
 
 void Door::enter(Character cCharacter) { // Function to allow a character to enter the door.
-	
+
+	cCharacter.SetArea(2); // This appears to be the problem. For some reason, calling cCharacter.SetArea() from Door.cpp doesn't work.
+	std::cout << "cCharacter's name: " << cCharacter.GetName() << std::endl; // However, it does not appear to be a problem with cCharacter, as this works fine.
+/*
 	if (cCharacter.GetArea()==GetSide1()) { // If the character is on the first side of the door...
 		cCharacter.SetArea(GetSide2()); // Bring them to the second side.
-		std::cout << "2You enter the door." << std::endl; } // And tell them that.
+		std::cout << "2You enter the door, going to Area " << GetSide2() << std::endl; } // And tell them that.
 	
 	else {
-	
+
+
+
 		if (cCharacter.GetArea()==GetSide2()) { // If the character is on the second side of the door...
 			cCharacter.SetArea(GetSide1()); // Bring them to the first side.
-			std::cout << "1You enter the door." << std::endl; } // And tell them that.
-		
+			std::cout << "1You enter the door, going to Area " << GetSide1() << std::endl;  // And tell them that.
+	}
 		else { // If the character is not on the first or second side of the door...
 			std::cout << "ERROR: Player is not on either side of door." << std::endl; // Tell them that crazy shit be goin on.
 			std::cout << "I'd exit() right now but I can't." << std::endl; } // Halt removed. Replaced with message.
 		}
+*/
+
 }
