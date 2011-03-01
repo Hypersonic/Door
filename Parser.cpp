235c9c398944ac37eea7d83583ad0a537dc14998
Josh Hofing <joshhofing@gmail.com>
2011-02-28 22:33:10 -0500

    added skeleton Parser class and header. The inputs/outputs for the various functions might change, for now they're pretty much all strings.

--- Parser.cpp
+++ Parser.cpp
@@ -11,9 +11,25 @@
  *
  */
 
-
+#include <iostream>
 #include <string>
 #include "Parser.h"
+void Parser::Parse (std::string input){
+	std::string output = Tokenize(input);
+	output = TokenParser(output);
+	ExecuteInput(output);
+}
 
+std::string Parser::Tokenize (std::string input){
+	std::string output = input;	//Placeholder
+	 return output;
+ }
 
+std::string Parser::TokenParser (std::string input) {
+	std::string output = input;	//Placeholder
+	return output;
+}
 
+void Parser::ExecuteInput (std::string input) {
+	std::string output = input;	//Placeholder
+}
--- Parser.h
+++ Parser.h
@@ -2,7 +2,26 @@
  *  Parser.h
  *  door
  *
- *  Created by Josh Hofing on 2/28/11.
+ *  You should only have to call Parse(inputString) with this, and it should do all the work for you.
+ *	i.e.: Game loop takes input, puts in it Parser.Parse(input) and then assumes any necessary parsing has been done.
+ *	
  *
  */
+#ifndef PARSER_H
+#define PARSER_H
 
+class Parser {
+
+private:
+	std::string Tokenize (std::string input);
+	std::string TokenParser (std::string input);
+	void ExecuteInput (std::string input);
+
+public:
+	
+	void Parse (std::string input);
+
+
+
+};
+#endif
