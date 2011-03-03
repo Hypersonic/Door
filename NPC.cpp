2af71cc66347f126ce5355f40b5f7c2c23f76930
Josh Hofing <joshhofing@gmail.com>
2011-03-03 13:33:29 -0500

    Added bare NPC files, which the various NPC's should be derived from

---
+++ NPC.cpp
@@ -0,0 +1,16 @@
+/*
+ *  NPC.cpp
+ *  door
+ *	
+ *	This class is the class that contains all AI functions.
+ *	It has to include AI for:
+ *	 Reactions to people based on a "Friendliness" factor, preferable 0-100
+ *	 Intelligent fighting AI based on the weapons that the NPC has. It has to be strategic!
+ *	 Perhaps an "Intelligence" factor, 1-100, that changes how the NPC reacts to variou circumstances
+ *	 AI to decide where to go next, if there is nothing else to do. There should also be a potential of staying in the same place.
+ *	 AI for handling conversation (this will be tricky to implement, but we want the AI to be interesting to talk to. Perhaps see a chatbot algorithm?)
+ *
+ */
+
+#include "NPC.h"
+
